5a49af0ff307845ee478758ad126eed2b079d665
Juan Ignacio Gonzalez <j.i.gonzalez24397@gmail.com>
2022-07-14 12:05:30 -0300

    posicionando la galeria

--- index.js
+++ index.js
@@ -6,7 +6,6 @@ const lightbox = document.querySelector('#contenedor-principal');
 const imagenActiva = document.querySelector('#img-activa');
 let indiceImagen = 0;
 
-/*Abre el Lightbox*/
 
 const abreLightbox = (event) => {
     imagenActiva.src = event.target.src;
@@ -18,13 +17,11 @@ imagenes.forEach((imagen) => {
     imagen.addEventListener('click', abreLightbox);
 });
 
-/*Cierra el Lightbox */
 
 btnCierra.addEventListener('click', () => {
     lightbox.style.display = 'none';
 });
 
-/* Adelanta la imagen*/
 
 const adelantaImagen = () => {
     if (indiceImagen === imagenes.length - 1) {
@@ -36,7 +33,6 @@ const adelantaImagen = () => {
 
 btnAdelanta.addEventListener('click', adelantaImagen);
 
-/*Retrocede la Imagen*/
 
 const retrocederImagen = () => {
     if (indiceImagen === 0) {
